26438462ca53995742cb39b5e6c3e1aac12990b6
carlitos9087 <carloseduardogomesalves@gmail.com>
2024-11-17 23:43:20 -0300

    responsividade ok

--- script.js
+++ script.js
@@ -177,7 +177,12 @@ function interpolatePoints(p1, p2, t) {
 
 
 
+const menuIcon = document.getElementById('menu-icon');
+const navMenu = document.getElementById('nav-menu');
 
+menuIcon.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
 
 
 
